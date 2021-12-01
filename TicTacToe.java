09440b46dc9f101373258cb710da8b33854a4caf
archish-edu <95363626+archish-edu@users.noreply.github.com>
2021-12-01 09:58:16 -0500

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,14 +1,30 @@
+import java.util.Scanner;
+
 public class TicTacToe {
-    // board is a 3x3 2d array initialized with numbers 1-9
+    static String[] board = {"1","2","3","4","5","6","7","9","9"};
+    static String player;
+
+    // board is a 1d array initialized with numbers 1-9
     public static void main(String[] args){
+        Scanner input  = new Scanner(System.in);
+
+        
+
         
         
 
     }
+    public static void printBoard(String[] board){
+        System.out.println(" "+board[0]+" "+"|");
+    }
+    
+    
+
+
 }
 
 class cpu {
-    int predictBestMove(char[][] board){
+    int predictBestMove(String[][] board){
         int place = 0; // will return from 1-9 if valid move available
 
         // if 2 in a row already, place in 3rd spot
@@ -22,13 +38,6 @@ class cpu {
 
 }
 
-class board {
-    
-    board() {
-        char[][] boardInit = {  {'1','2','3'}, 
-                                {'4','5','6'}, 
-                                {'7','8','9'}   };
-    }
 
-}
+
 
