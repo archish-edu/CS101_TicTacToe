cdd14600e4bfbc83819e9b4d0be65601591bb650
archish-edu <95363626+archish-edu@users.noreply.github.com>
2021-12-01 09:49:48 -0500

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,3 +1,34 @@
 public class TicTacToe {
-    
+    // board is a 3x3 2d array initialized with numbers 1-9
+    public static void main(String[] args){
+        
+        
+
+    }
 }
+
+class cpu {
+    int predictBestMove(char[][] board){
+        int place = 0; // will return from 1-9 if valid move available
+
+        // if 2 in a row already, place in 3rd spot
+        // else if 1 placed already, place in 2nd spot
+        // else place in first available random spot 
+
+        return place;
+
+
+    }
+
+}
+
+class board {
+    
+    board() {
+        char[][] boardInit = {  {'1','2','3'}, 
+                                {'4','5','6'}, 
+                                {'7','8','9'}   };
+    }
+
+}
+
